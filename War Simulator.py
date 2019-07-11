e6bcd715512d17ba1168ef3b49f35b509a31f991
Daniel Shamtob <48531579+dshamtob@users.noreply.github.com>
2019-07-10 19:34:50 -0700

    Update War Simulator.py

--- War Simulator.py
+++ War Simulator.py
@@ -31,18 +31,22 @@ hand1=cards[:26]
 hand2=cards[-26:]
 
 #handles cases where each player plays the same card
+#1 war is type 0, 2 wars in a row is type 2, 3 wars in a row is type 4, and so on
 def war(type):
 	print("WAR!")
 	print(hand1[type] + " and " + hand2[type] + " were played")
-
+	
+	#determines raw values of cards compared in war (raw values range from 0-12)
 	warCard1=cardso.index(hand1[type + 2])
 	warCard2=cardso.index(hand2[type + 2])
 	while warCard1>12:
 		warCard1-=13
 	while warCard2>12:
 		warCard2-=13
-
+	
+	#handles case where card 1 wins war
 	if warCard1>warCard2:
+		#handles face down cards after war
 		x=1
 		savings = []
 		winnings = []
@@ -50,8 +54,11 @@ def war(type):
 			savings.append(hand1[x])
 			winnings.append(hand2[x])
 			x+=2
+			
 		print("player 1's " + hand1[type + 2] + " beat player 2's " + hand2[type + 2])		
 		print("player 1 saved " + str(savings) + " and player 2 lost " + str(winnings))
+		
+		#handling losses and gains
 		hand1.extend(hand1[:(type + 2)])
 		hand1.extend(hand2[:(type + 2)])
 		del hand1[:(type + 2)]
@@ -60,6 +67,7 @@ def war(type):
 		winnings = []
 
 	elif warCard2>warCard1:
+		#handles face down cards after war
 		x=1
 		savings = []
 		winnings = []
@@ -67,20 +75,26 @@ def war(type):
 			savings.append(hand2[x])
 			winnings.append(hand1[x])
 			x+=2
+			
 		print("player 2's " + hand2[type + 2] + " beat player 1's " + hand1[type + 2])		
 		print("player 2 saved " + str(savings) + " and player 1 lost " + str(winnings))
+		
+		#handling losses and gains
 		hand2.extend(hand1[:(type + 2)])
 		hand2.extend(hand2[:(type + 2)])
 		del hand1[:(type + 2)]
 		del hand2[:(type + 2)]
 		savings = []
 		winnings = []
-
+		
+	#if instead of the war ending, a second war occurs
 	else:
 		type+=2
 		war(type)
 
+#runs the game under the condition that both players still have cards
 while len(hand1)>0 and len(hand2)>0:
+	#determines raw values of the two cards played (raw values range from 0-12)
 	card1=cardso.index(hand1[0])
 	card2=cardso.index(hand2[0])
 	while card1>12:
@@ -107,9 +121,11 @@ while len(hand1)>0 and len(hand2)>0:
 			war(0)
 		except IndexError:
 				if len(hand1)<len(hand2):
+					print("player 1 does not have enough cards to compete in war")
 					print("player 2 won in " + str(turns) + " turns")
 					break
 				else:
+					print("player 2 does not have enough cards to compete in war")
 					print("player 1 won in " + str(turns) + " turns")
 					break
 if len(hand1)==0:
